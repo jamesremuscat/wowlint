86d612165869df01836b8796a1fa837b37e83dd2
James Muscat <jamesremuscat@gmail.com>
2016-08-01 17:22:47 +0100

    Add commandline option to show only errors.

--- src/wowlint/command.py
+++ src/wowlint/command.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 from wowlint.linter import Linter
+from wowlint.validation.core import Severity
 
 
 def main():
     parser = argparse.ArgumentParser(description='Lint and validate Words of Worship resource files.')
     parser.add_argument('file', nargs='+', help='File(s) to validate')
+    parser.add_argument('-e', '--errors-only', help='Only show errors, not warnings')
     args = parser.parse_args()
 
     issues = []
@@ -20,7 +22,8 @@ def main():
             longestFileName = max(longestFileName, len(subject))
 
     for issue in issues:
-        print "{:{width}}: {:8} {}".format(issue.sourceFile, issue.severity, issue.message, width=longestFileName + 2)
+        if args.errors_only is None or issue.severity == Severity.ERROR:
+            print "{:{width}}: {:8} {}".format(issue.sourceFile, issue.severity, issue.message, width=longestFileName + 2)
 
 if __name__ == "__main__":
     main()
