135d78e8d971efdc66889420764214dc115d5a13
James Muscat <jamesremuscat@gmail.com>
2016-08-02 11:46:00 +0100

    Don't buffer issues, print them immediately.

--- src/wowlint/command.py
+++ src/wowlint/command.py
@@ -13,8 +13,6 @@ def main():
     parser.add_argument('-l', '--list', action='store_true', help='Only list files, not error details')
     args = parser.parse_args()
 
-    issues = []
-
     linter = Linter()
 
     longestFileName = 0
@@ -24,22 +22,21 @@ def main():
 
     for subject in args.file:
         if os.path.isfile(subject):
-            subjectIssues = linter.lint(subject, minSeverity)
-            issues += subjectIssues
-            if len(subjectIssues) > 0:
-                highestSeverityEncountered = max(max(map(lambda i: i.severity, subjectIssues), highestSeverityEncountered))
-            longestFileName = max(longestFileName, len(subject))
+            issues = linter.lint(subject, minSeverity)
+            if len(issues) > 0:
+                highestSeverityEncountered = max(max(map(lambda i: i.severity, issues), highestSeverityEncountered))
+                longestFileName = max(longestFileName, len(subject))
 
-    if args.list:
-        files = []
-        for f in map(lambda i: i.sourceFile, issues):  # Deduplicate maintaining ordering
-            if f not in files:
-                files.append(f)
-        for f in files:
-            print f
-    else:
-        for issue in issues:
-            print "{:{width}}: {:8} {}".format(issue.sourceFile, issue.severity, issue.message, width=longestFileName + 2)
+                if args.list:
+                    print subject
+                else:
+                    for issue in issues:
+                        print "{:{width}}: {:8} {}".format(
+                            issue.sourceFile,
+                            issue.severity,
+                            issue.message,
+                            width=longestFileName + 2
+                        )
 
     if highestSeverityEncountered >= Severity.ERROR:
         sys.exit(1)
