db097700e73f643e27de707e55d69402f7da1d38
James Muscat <jamesremuscat@gmail.com>
2016-08-03 17:28:00 +0100

    Refactor "should run" into its own method.

--- src/wowlint/linter.py
+++ src/wowlint/linter.py
@@ -19,14 +19,17 @@ class Linter(object):
                 try:
                     song = Song.parse(f.read())
                     for lint in SONG_LINTS:
-                        if lint.__class__.__name__ in self.config:
-                            lintConfig = self.config[lint.__class__.__name__]
-                            if "exclude" in lintConfig and filename in lintConfig["exclude"]:
-                                continue
-                        if lint.severity >= self.minSeverity:
+                        if lint.severity >= self.minSeverity and self._shouldLintFile(lint, filename):
                             issues += lint.validate(song)
                 except ConstructError as e:
                     Issue(Severity.FATAL, "{} Not a valid Words of Worship song file".format(e.__class__.__name__)).add_to(issues)
             else:
                 Issue(Severity.INFO, "Unrecognised file extension: {}".format(os.path.splitext(filename)[1])).add_to(issues)
         return issues
+
+    def _shouldLintFile(self, lint, filename):
+        if lint.__class__.__name__ in self.config:
+            lintConfig = self.config[lint.__class__.__name__]
+            if "exclude" in lintConfig and filename in lintConfig["exclude"]:
+                return False
+        return True
