1f9c2b9c6afaa26c46dd4d58744301d5a3eae34e
James Muscat <jamesremuscat@gmail.com>
2016-08-04 15:34:21 +0100

    Keep a map of known file extensions to models and lints.

--- src/wowlint/linter.py
+++ src/wowlint/linter.py
@@ -13,19 +13,26 @@ class Linter(object):
 
         self.songLints = map(lambda l: l(config.get(l.__name__, {})), SONG_LINT_CLASSES)
 
+        self.known_files = {
+            '.wow-song': (Song, self.songLints),
+            '.wsg': (Song, self.songLints),
+        }
+
     def lint(self, filename):
         issues = []
-        with open(filename, "rb") as f:
-            if filename.endswith(".wow-song") or filename.endswith(".wsg"):
+        ext = os.path.splitext(filename)[1]
+        if ext in self.known_files:
+            with open(filename, "rb") as f:
                 try:
-                    song = Song.parse(f.read())
-                    for lint in self.songLints:
+                    modelType, lints = self.known_files[ext]
+                    model = modelType.parse(f.read())
+                    for lint in lints:
                         if lint.severity >= self.minSeverity and self._shouldLintFile(lint, filename):
-                            issues += lint.validate(song)
+                            issues += lint.validate(model)
                 except ConstructError as e:
-                    Issue(Severity.FATAL, "{} Not a valid Words of Worship song file".format(e.__class__.__name__)).add_to(issues)
-            else:
-                Issue(Severity.INFO, "Unrecognised file extension: {}".format(os.path.splitext(filename)[1])).add_to(issues)
+                    Issue(Severity.FATAL, "{} Not a valid Words of Worship file".format(e.__class__.__name__)).add_to(issues)
+        else:
+            Issue(Severity.INFO, "Unrecognised file extension: {}".format(os.path.splitext(filename)[1])).add_to(issues)
         return issues
 
     def _shouldLintFile(self, lint, filename):
--- src/wowlint/tests/testlinter.py
+++ src/wowlint/tests/testlinter.py
@@ -2,6 +2,7 @@ from unittest.case import TestCase
 
 from wowlint.linter import Linter
 from wowlint.tests.utils import getTestFilePath
+from wowlint.validation.core import Severity, Issue
 
 
 class TestLinter(TestCase):
@@ -19,3 +20,13 @@ class TestLinter(TestCase):
         results = linter.lint(testFile)
 
         self.assertEqual([], results)
+
+    def testUnknownFileExtensions(self):
+        linter = Linter()
+        results = linter.lint("unknown.ext")
+        self.assertEqual(
+            [
+                Issue(Severity.INFO, "Unrecognised file extension: .ext")
+            ],
+            results
+        )
